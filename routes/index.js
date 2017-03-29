f7debc25a7e8cbe23ac150a80b835f70547a19bb
Bruno Farias <bruno.cabral88@hotmail.com>
2017-03-29 19:48:31 -0400

    Login and logout functionalityes along with DataHelpers functions

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,48 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
 
-router.get('/login',(req,res) => {
-	res.render('login');
-});
+module.exports = function(knex) {
+	const DataHelpers = require('../lib/DataHelpers')(knex);
 
-router.post('/login',(req,res) => {
-	console.log(req.body.username);
-})
+	router.get('/login',(req,res) => {
+		res.render('login');
+	});
 
-router.get('/register',(req,res) => {
-	res.render('register');
-})
+	router.post('/login',(req,res) => {
+		if (req.body.username && req.body.password) {
+			DataHelpers.loginUser(req.body.username,req.body.password,function(err,userObj){
+				if (err) console.log(err);
+				if (userObj) {
+					req.session.user_id = userObj.id;
+					req.session.email = userObj.email;
+					req.session.name = userObj.name;
+					res.redirect('/homepage.html');
+				} else {
+					res.redirect('/login');
+				}
+			});
+		}
+	});
 
-module.exports = router;
+	router.get('/register',(req,res) => {
+		res.render('register');
+	});
+
+	router.post('/register',(req,res) => {
+		if (req.body.password === req.body.password2 && (req.body.email && req.body.name)) {
+			DataHelpers.insertUser(req.body.name,req.body.email,req.body.password,(err,arrIds) => {
+				if (err) throw err;
+				req.session.user_id = arrIds[0];
+				req.session.email = req.body.email;
+				req.session.name = req.body.name;
+				res.redirect('/login');
+			});
+		} else {
+			res.redirect('/register');
+		}
+		
+	});
+	
+	return router;
+}
