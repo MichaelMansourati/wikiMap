7300d0cfb5fa3148195962233766cca728302918
Bruno Cabral <bruno.cabral88@hotmail.com>
2017-03-29 19:54:10 +0000

    Adds bcrypt, login page and register page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/login',(req,res) => {
+	res.render('login');
 });
+
+router.post('/login',(req,res) => {
+	console.log(req.body.username);
+})
+
+router.get('/register',(req,res) => {
+	res.render('register');
+})
 
 module.exports = router;
